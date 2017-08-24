7ff445234161ea1ab2edea16ed215f9140948474
符坤 <fukun@fukundeMacBook-Pro.local>
2017-08-24 08:41:32 +0800

    modify listener start

--- dylenfu.go
+++ dylenfu.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/Loopring/ringminer/p2p/ipfs"
+	"time"
 )
 
 func DebugOrderBook() {
 	listener := ipfs.NewListener("topic")
 	listener.Start()
+	time.Sleep(10 * time.Second)
+	listener.Stop()
 }
--- p2p/ipfs/p2p-listener.go
+++ p2p/ipfs/p2p-listener.go
@@ -27,17 +27,17 @@ func NewListener(topic string) *IPFSListener{
 
 func (listener *IPFSListener) Start() {
 	listener.quit <- true
-	for {
-		record, _ := listener.sub.Next()
-		peerId := record.From().String()
-		topics := record.TopicIDs()
-		seqNum := record.SeqNo()
-		data := record.Data()
+	go func() {
+		for {
+			record, _ := listener.sub.Next()
+			peerId := record.From().String()
+			data := record.Data()
 
-		if len(topics) > 0 && seqNum > 0 && len(peerId) > 0 {
-			log.Printf("p2p listener\t- Listen peerId %s,topic %s, seq number %d,data %s", peerId, topics[0], seqNum, string(data))
+			if len(peerId) > 0 {
+				log.Printf("p2p listener\t- Listen peerId %s,data %s", peerId, string(data))
+			}
 		}
-	}
+	}()
 }
 
 func (listener *IPFSListener) Stop() {
