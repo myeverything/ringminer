5d807730f3032210ca306f4e5ae6cd1be2ebc3c6
于红雨 <jim@umei.me>
2017-09-13 10:50:14 +0800

    optimize

--- matchengine/ringclient.go
+++ matchengine/ringclient.go
@@ -1,3 +1,21 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
 package matchengine
 
 import (
@@ -62,9 +80,9 @@ func sendRing() {
 		println(filterId)
 	}
 	//todo：取消该filterId的订阅
-	defer func() {
-		eth.EthClient.UninstallFilter()
-	}()
+	//defer func() {
+	//	eth.EthClient.UninstallFilter()
+	//}()
 
 	logChan := make(chan []eth.Log)
 	if err := eth.EthClient.Subscribe(&logChan, filterId);nil != err {
@@ -85,7 +103,7 @@ func sendRing() {
 						} else {
 							//标记为已删除,迁移到已完成的列表中
 							unSubmitedRingsStore.Delete(ringHash)
-							submitedRingsStore.Put(ringHash, ring)
+							//submitedRingsStore.Put(ringHash, ring)
 						}
 					} else {
 						println(err.Error())
--- types/gen_ring_json.go
+++ types/gen_ring_json.go
@@ -16,22 +16,7 @@
 
 */
 
-package matchengine_test
+package types
 
-import (
-	"testing"
-	"github.com/Loopring/ringminer/types"
-	"github.com/Loopring/ringminer/matchengine"
-)
 
-func TestRingClient(t *testing.T) {
-
-	ring := &types.RingState{}
-	hash := &types.Hash{}
-	hash.SetBytes([]byte("testtesthash"))
-	ring.Hash = *hash
-	ring.FeeMode = 1
-
-	matchengine.NewRing(ring)
-}
 
